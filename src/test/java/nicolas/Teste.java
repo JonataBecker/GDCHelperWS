03e905015c54fa6260639b56bf850983baacd914
Jouwee <nicolaspohren@gmail.com>
2017-08-15 23:18:10 -0300

    Passa a dividir o arquivo de frases classificadas em treinamento e teste para termos estatísticas de precisão

--- src/test/java/nicolas/Teste.java
+++ src/test/java/nicolas/Teste.java
@@ -1,6 +1,8 @@
 package nicolas;
 
 import com.github.gdchelper.gdchelperws.SentencePreprocessor;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,6 +12,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import opennlp.tools.doccat.DoccatModel;
 import opennlp.tools.doccat.DocumentCategorizerME;
 import opennlp.tools.doccat.DocumentSampleStream;
@@ -22,19 +25,32 @@ import org.apache.commons.csv.CSVRecord;
 
 public class Teste {
 
+    static final String DADOS = "c:\\users\\pichau\\desktop\\exportar.csv";
+    static final String SENTIMENT = "D:\\Projects\\GDCHelper\\GDCHelperWS\\src\\main\\java\\com\\github\\gdchelper\\gdchelperws\\models\\sentiment.bin";
+    static final String SENTENCER = "D:\\Projects\\GDCHelper\\GDCHelperWS\\src\\main\\java\\com\\github\\gdchelper\\gdchelperws\\models\\pt-sent.bin";
     static final int LIMITE = 3000;
     static final int TAMANHO_MINIMO = 5;
+    static final double TREINAMENTO = 0.8;
 
     public static void main(String[] args) throws Exception {
-        Teste teste = new Teste();
-        long l = System.currentTimeMillis();
-        List<Atendimento> atendimentos = teste.loadAtendimentos();
-        System.out.println(atendimentos.size() + " atendimentos carregados em " + (System.currentTimeMillis() - l) + "ms");
-
+        Teste t = new Teste();
+        List<Atendimento> atendimentos = t.loadAtendimentos();
+        List<FraseTreinamento> classificados = t.loadTreinamentos();
+        
+        // TESTE: USA SÓ BOM E RUIM
+        classificados = classificados.stream().map((frase) -> new FraseTreinamento(frase.getCategoria().replaceFirst("^m", ""), frase.getFrase())).collect(Collectors.toList());
+        // TESTE: IGNORA NEUTRO
+        classificados = classificados.stream().filter((frase) -> !frase.getCategoria().equals("neutro")).collect(Collectors.toList());
+        
+        int trainingIterations = (int) (classificados.size() * TREINAMENTO);
+        List<FraseTreinamento> treinamento = classificados.subList(0, trainingIterations);
+        List<FraseTreinamento> teste = classificados.subList(trainingIterations, classificados.size());
+        
+        System.out.println(treinamento.size() + " registros de treinamento");
+        System.out.println(teste.size() + " registros de teste");
+        
         int cutoff = 2;
-        int trainingIterations = 34;
-        InputStream dataIn = new FileInputStream("D:\\Projects\\GDCHelper\\GDCHelperWS\\src\\main\\java\\com\\github\\gdchelper\\gdchelperws\\models\\sentiment.bin");
-        ObjectStream lineStream = new PlainTextByLineStream(dataIn, "UTF-8");
+        ObjectStream lineStream = new PlainTextByLineStream(t.getStreamFrom(treinamento), "UTF-8");
         ObjectStream sampleStream = new DocumentSampleStream(lineStream);
         DoccatModel model = DocumentCategorizerME.train("pt", sampleStream, cutoff, trainingIterations);
         DocumentCategorizerME myCategorizer = new DocumentCategorizerME(model);
@@ -42,7 +58,7 @@ public class Teste {
         Map<String, Integer> totais = new HashMap<>();
         
         for (Atendimento atendimento : atendimentos) {
-            List<String> sentences = teste.extractSentences(atendimento.getTexto());
+            List<String> sentences = t.extractSentences(atendimento.getTexto());
 //            System.out.println("----------------------");
             for (int i = 0; i < sentences.size(); i++) {
                 String sentence = sentences.get(i);
@@ -77,14 +93,32 @@ public class Teste {
         for (Map.Entry<String, Integer> entry : totais.entrySet()) {
             System.out.println(entry.getKey() + "\t" + entry.getValue());
         }
+
+        t.testa(teste, myCategorizer);
         
     }
 
+    private void testa(List<FraseTreinamento> teste, DocumentCategorizerME myCategorizer) {
+        int certos = 0;
+        int errados = 0;
+        for (FraseTreinamento fraseTreinamento : teste) {
+            double[] outcomes = myCategorizer.categorize(fraseTreinamento.getFrase());
+            String category = myCategorizer.getBestCategory(outcomes);
+            if (category.equals(fraseTreinamento.getCategoria())) {
+                certos++;
+            } else {
+                errados++;
+            }
+        }
+        System.out.println("Acertei " + certos + " (" + ((double)certos / (double)(certos + errados) * 100) + "%)");
+        System.out.println("Errei " + errados + " (" + ((double)errados / (double)(certos + errados) * 100) + "%)");
+    }
+    
     private List<String> extractSentences(String text) throws IOException {
         List<String> list = new ArrayList<>();
         // always start with a model, a model is learned from training data
 //      InputStream is = Teste.class.getResourceAsStream("/com/github/gdchelper/gdchelperws/models/pt-sent.bin");
-        InputStream is = new FileInputStream("D:\\Projects\\GDCHelper\\GDCHelperWS\\src\\main\\java\\com\\github\\gdchelper\\gdchelperws\\models\\pt-sent.bin");
+        InputStream is = new FileInputStream(SENTENCER);
         SentenceModel model = new SentenceModel(is);
         SentenceDetectorME sdetector = new SentenceDetectorME(model);
 
@@ -125,7 +159,7 @@ public class Teste {
     private List<Atendimento> loadAtendimentos() throws IOException {
         List<Atendimento> atendimentos = new ArrayList<>();
         int i = 0;
-        try (FileReader reader = new FileReader("c:\\users\\pichau\\desktop\\exportar.csv")) {
+        try (FileReader reader = new FileReader(DADOS)) {
             Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(reader);
             for (CSVRecord record : records) {
                 if (i == 0) { // Pula cabeçalho
@@ -144,6 +178,31 @@ public class Teste {
         return atendimentos;
     }
 
-
+    private List<FraseTreinamento> loadTreinamentos() throws IOException {
+        List<FraseTreinamento> treinamentos = new ArrayList<>();
+        int i = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(SENTIMENT))) {
+            while (true) {
+                String l = reader.readLine();
+                if (l == null) {
+                    break;
+                }
+                String[] parts = l.split("\\s+", 2);
+                treinamentos.add(new FraseTreinamento(parts[0], new SentencePreprocessor().process(parts[1])));
+            }
+        }
+        return treinamentos;
+    }
+    
+    private InputStream getStreamFrom(List<FraseTreinamento> treinamento) {
+        StringBuilder buffer = new StringBuilder();
+        for (FraseTreinamento fraseTreinamento : treinamento) {
+            buffer.append(fraseTreinamento.getCategoria());
+            buffer.append('\t');
+            buffer.append(fraseTreinamento.getFrase());
+            buffer.append('\n');
+        }
+        return new ByteArrayInputStream(buffer.toString().getBytes());
+    }
 
 }
