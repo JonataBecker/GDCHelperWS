5e7338837a7770956b55b3f1e8c3a2f65774cd8e
JonataBecker <jonatabecker@gmail.com>
2017-09-02 18:10:20 -0300

    Importação de atendimentos

--- src/main/java/com/github/gdchelper/db/DataFileReader.java
+++ src/main/java/com/github/gdchelper/db/DataFileReader.java
@@ -3,8 +3,13 @@ package com.github.gdchelper.db;
 import com.github.gdchelper.jpa.Atendimento;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 
@@ -13,6 +18,8 @@ import org.apache.commons.csv.CSVRecord;
  * @author JonataBecker
  */
 public class DataFileReader {
+    
+    private static final SimpleDateFormat FORMAT_DATE = new SimpleDateFormat("dd/MM/yy hh:mm:ss");
     
     private final int start;
     private final int end;
@@ -41,9 +48,14 @@ public class DataFileReader {
                     continue;
                 }
                 Atendimento atendimento = new Atendimento();
-                atendimento.setCliente(record.get(21));
-                atendimento.setTecnico(record.get(4));
+                atendimento.setCliente(getInt(record.get(21)));
+                atendimento.setTecnico(getInt(record.get(4)));
                 atendimento.setMensagem(record.get(3));
+                atendimento.setData(getDate(record.get(34)));
+                atendimento.setDataInicio(getDate(record.get(23)));
+                atendimento.setDataFim(getDate(record.get(27)));
+                atendimento.setSegundos(getInt(record.get(37)));
+                atendimento.setSistema(record.get(22));
                 atendimentos.add(atendimento);
                 if (i++ > end) {
                     break;
@@ -52,5 +64,27 @@ public class DataFileReader {
         }
         return atendimentos;
     }
+    
+    private int getInt(String value) {
+        if (isEmpty(value)) {
+            return 0;
+        }
+        return Integer.valueOf(value);
+    }
 
+    private Date getDate(String value) {
+        try {
+            if (!isEmpty(value)) {
+                return FORMAT_DATE.parse(value);
+            }
+        } catch (ParseException ex) {
+            Logger.getLogger(DataFileReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    private boolean isEmpty(String value) {
+        return value == null || value.isEmpty();
+    }
+    
 }
