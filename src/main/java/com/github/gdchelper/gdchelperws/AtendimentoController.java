1280bb9eee16da9d7e67c3add5eb196e6fecb37e
JonataBecker <jonatabecker@gmail.com>
2017-09-23 08:16:19 -0300

    Ajuste busca de histórico de atendimentos

--- src/main/java/com/github/gdchelper/gdchelperws/AtendimentoController.java
+++ src/main/java/com/github/gdchelper/gdchelperws/AtendimentoController.java
@@ -4,15 +4,10 @@ import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.view.Results;
-import com.github.gdchelper.jpa.Atendimento;
 import com.github.gdchelper.jpa.PersistenceManager;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
 /**
  *
@@ -23,76 +18,27 @@ public class AtendimentoController {
 
     private final Result result;
     private final PersistenceManager persistenceManager;
+    private final HistoricoAntedimentos historicoAntedimentos;
 
     /**
      * @deprecated CDI eyes only
      */
     public AtendimentoController() {
-        this(null, null);
+        this(null, null, null);
     }
 
     @Inject
-    public AtendimentoController(Result result, PersistenceManager persistenceManager) {
+    public AtendimentoController(Result result, PersistenceManager persistenceManager, HistoricoAntedimentos historicoAntedimentos) {
         this.result = result;
         this.persistenceManager = persistenceManager;
+        this.historicoAntedimentos = historicoAntedimentos;
     }
 
-    @Get("/atendimentos")
-    public void atendimento() {
-        EntityManager em = persistenceManager.create();
-        try {
-            Query q = em.createQuery("SELECT a FROM Atendimento a");
-            List<Atendimento> userList = q.getResultList();
-            result.use(Results.json()).withoutRoot().from(userList).serialize();
-        } finally {
-            em.close();
-        }
-    }
-    
     @Get("/atendimentos/historico")
     public void atendimentoHistorico(String dataInicial, String dataFinal, String gdc, String cliente) {
         EntityManager em = persistenceManager.create();
         try {
-            String sql = "SELECT \n"
-                    + "	CONCAT(Atendimento.cliente, '-', Cliente.nome) AS Cliente, \n"
-                    + "	COUNT(*) AS quantidade, \n"
-                    + "	FORMAT(SUM(Segundos / 60 / 60), 2) AS tempo, \n"
-                    + " MAX(Tecnico.apelido),"
-                    + " Atendimento.cliente as Codigo_Cliente "
-                    + "FROM Atendimento \n"
-                    + "INNER JOIN Cliente\n"
-                    + "	ON (Atendimento.cliente = Cliente.codigo) \n"
-                    + "INNER JOIN Tecnico\n"
-                    + "	ON (Cliente.gdc = Tecnico.codigo)\n"
-                    + "WHERE \n"
-                    + "	Atendimento.dataInicio >= :dataInicio AND\n"
-                    + "	Atendimento.dataFim <= :dataFim AND\n"
-                    + "	Tecnico.apelido LIKE :gdc AND \n"
-                    + "	Cliente.nome LIKE :cliente \n"
-                    + "GROUP BY Atendimento.Cliente \n"
-                    + "ORDER BY Quantidade DESC, Tempo DESC";
-            List list = new ArrayList();
-            if (gdc == null) {
-                gdc = "";
-            }
-            if (cliente == null) {
-                cliente = "";
-            }
-            Query q = em.createNativeQuery(sql);
-            q.setParameter("dataInicio", dataInicial);
-            q.setParameter("dataFim", dataFinal);
-            q.setParameter("gdc", "%" + gdc + "%");
-            q.setParameter("cliente", "%" + cliente + "%");
-            q.getResultList().forEach((obj) -> {
-                Object[] item = (Object[]) obj;
-                Map resultMap = new LinkedHashMap();
-                resultMap.put("cliente", item[0]);
-                resultMap.put("quantidade", item[1]);
-                resultMap.put("tempo", item[2]);
-                resultMap.put("gdc", item[3]);
-                resultMap.put("idCliente", item[4]);
-                list.add(resultMap);
-            });
+            List list = historicoAntedimentos.getAtendimentos(em, dataInicial, dataFinal, gdc, cliente);
             result.use(Results.json()).withoutRoot().from(list).serialize();
         } finally {
             em.close();
@@ -103,32 +49,7 @@ public class AtendimentoController {
     public void atendimentoHistoricoContato(int idCliente, String dataInicial, String dataFinal) {
         EntityManager em = persistenceManager.create();
         try {
-            String sql = "SELECT \n" +
-                        "  COUNT(*) AS quantidade, \n" +
-                        "  FORMAT(SUM(Segundos / 60 / 60), 2) AS tempo, \n" +
-                        "  IFNULL(Contato.nome, 'Não definido')\n" +
-                        "FROM Atendimento\n" +
-                        "  LEFT JOIN Contato\n" +
-                        "    ON (Contato.id = Atendimento.contato)\n" +
-                        "WHERE \n" + 
-                        "  Atendimento.dataInicio >= :dataInicio AND\n" +
-                        "  Atendimento.dataFim <= :dataFim AND\n" + 
-                        "  Atendimento.cliente = :cliente \n" + 
-                        "GROUP BY Atendimento.contato\n" +
-                        "ORDER BY Quantidade DESC, Tempo DESC";
-            List list = new ArrayList();
-            Query q = em.createNativeQuery(sql);
-            q.setParameter("dataInicio", dataInicial);
-            q.setParameter("dataFim", dataFinal);
-            q.setParameter("cliente", idCliente);
-            q.getResultList().forEach((obj) -> {
-                Object[] item = (Object[]) obj;
-                Map resultMap = new LinkedHashMap();
-                resultMap.put("quantidade", item[0]);
-                resultMap.put("tempo", item[1]);
-                resultMap.put("contato", item[2]);
-                list.add(resultMap);
-            });
+            List list = historicoAntedimentos.getContato(em, idCliente, dataInicial, dataFinal);
             result.use(Results.json()).withoutRoot().from(list).serialize();
         } finally {
             em.close();
@@ -139,32 +60,7 @@ public class AtendimentoController {
     public void atendimentoHistoricoSistema(int idCliente, String dataInicial, String dataFinal) {
         EntityManager em = persistenceManager.create();
         try {
-            String sql = "SELECT \n" +
-                        "  COUNT(*) AS quantidade, \n" +
-                        "  FORMAT(SUM(Segundos / 60 / 60), 2) AS tempo, \n" +
-                        "  IFNULL(SistemaContratado.descricao, 'Não definido')\n" +
-                        "FROM Atendimento\n" +
-                        "  LEFT JOIN SistemaContratado \n" +
-                        "    ON (SistemaContratado.codigoSistema = Atendimento.sistema)\n" +
-                        "WHERE \n" + 
-                        "  Atendimento.dataInicio >= :dataInicio AND\n" +
-                        "  Atendimento.dataFim <= :dataFim AND\n" + 
-                        "  Atendimento.cliente = :cliente \n" + 
-                        "GROUP BY SistemaContratado.descricao \n" +
-                        "ORDER BY Quantidade DESC, Tempo DESC";
-            List list = new ArrayList();
-            Query q = em.createNativeQuery(sql);
-            q.setParameter("dataInicio", dataInicial);
-            q.setParameter("dataFim", dataFinal);
-            q.setParameter("cliente", idCliente);
-            q.getResultList().forEach((obj) -> {
-                Object[] item = (Object[]) obj;
-                Map resultMap = new LinkedHashMap();
-                resultMap.put("quantidade", item[0]);
-                resultMap.put("tempo", item[1]);
-                resultMap.put("sistema", item[2]);
-                list.add(resultMap);
-            });
+            List list = historicoAntedimentos.getSitema(em, idCliente, dataInicial, dataFinal);
             result.use(Results.json()).withoutRoot().from(list).serialize();
         } finally {
             em.close();
