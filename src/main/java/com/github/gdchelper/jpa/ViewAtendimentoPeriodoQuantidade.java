240dbdacca318c552848ffc55f1e68548bc758fe
JonataBecker <jonatabecker@gmail.com>
2017-09-30 14:53:52 -0300

    Criado consulta de usuários do sistema

--- src/main/java/com/github/gdchelper/jpa/ViewAtendimentoPeriodoQuantidade.java
+++ src/main/java/com/github/gdchelper/jpa/ViewAtendimentoPeriodoQuantidade.java
@@ -1,26 +1,30 @@
 package com.github.gdchelper.jpa;
 
 import java.io.Serializable;
-import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
+import jdk.nashorn.internal.ir.annotations.Immutable;
 
 /**
  *
  * @author JonataBecker
  */
+@Entity
+@Immutable
+@Table(name = "View_Atendimento_Periodo_Quantidade")
 public class ViewAtendimentoPeriodoQuantidade implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Periodo", updatable = false, nullable = false)
     private String periodo;
+    @Column(name = "Periodo_USA")
     private String periodoUSA;
+    @Column(name = "Quantidade")
     private long quantidade;
 
     public ViewAtendimentoPeriodoQuantidade() {
