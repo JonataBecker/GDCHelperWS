cd8b42c4182f27f4d98484340defa2583422fedd
marinalaux <laux.marina@gmail.com>
2017-10-24 22:26:56 -0200

    Disponibilizada lista de sistemas disponíveis para contratação.

---
+++ src/main/java/com/github/gdchelper/gdchelperws/SistemaController.java
@@ -0,0 +1,46 @@
+package com.github.gdchelper.gdchelperws;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.view.Results;
+import com.github.gdchelper.jpa.PersistenceManager;
+import com.github.gdchelper.jpa.SistemaContratado;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+@Controller
+public class SistemaController {
+
+    private final Result result;
+    private final PersistenceManager persistenceManager;
+
+    /**
+     * @deprecated CDI eyes only
+     */
+    public SistemaController() {
+        this(null, null);
+    }
+
+    @Inject
+    public SistemaController(Result result, PersistenceManager persistenceManager) {
+        this.result = result;
+        this.persistenceManager = persistenceManager;
+    }
+
+    @Get("/sistemas")
+    public void listaSistemasDisponiveis() {
+        EntityManager em = persistenceManager.create();
+        try {
+            Query q = em.createQuery("SELECT s.codigoSistema, s.descricao FROM SistemaContratado s GROUP BY s.codigoSistema");
+            List<SistemaContratado> userList = q.getResultList();
+            result.use(Results.json()).withoutRoot().from(userList).serialize();
+        } finally {
+            em.close();
+        }
+
+    }
+
+}
